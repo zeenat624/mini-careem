d9595587c7ac564e8cdc0ac11b44456ea0dba193
Zeenat <114605792+zeenat624@users.noreply.github.com>
2026-07-23 22:50:06 +0500

    Delete file

--- metadata-service/metadata/redis_client.py
+++
@@ -1,9 +0,0 @@
-import redis
-
-def get_redis():
-    try:
-        r = redis.Redis(host="redis", port=6379, decode_responses=True)
-        r.ping()
-        return r
-    except:
-        return None
