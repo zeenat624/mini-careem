55078ac262533d936a5aa8c7fe09881607bb2113
Zeenat <114605792+zeenat624@users.noreply.github.com>
2026-07-23 23:35:30 +0500

    adding test cases

--- metadata-service/test/test.js
+++ metadata-service/test/test.js
@@ -5,11 +5,19 @@ async function testMetadata() {
   const res = await fetch('http://localhost:8084/api/metadata');
   const data = await res.json();
 
-// check if values are there
+  // check fare and peak values exist
   assert(data.fare_rate !== undefined);
   assert(data.peak_multiplier !== undefined);
 
-  console.log("Test passed ");
+  // check values are numbers
+  assert(typeof data.fare_rate === 'number');
+  assert(typeof data.peak_multiplier === 'number');
+
+  // check values are positive
+  assert(data.fare_rate >= 0);
+  assert(data.peak_multiplier >= 0);
+
+  console.log("All tests passed ");
 }
 
 testMetadata();
